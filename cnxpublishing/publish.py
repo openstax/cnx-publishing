58ef498182c2630e6f00b0ab8f18ceca8ec99a65
Michael Mulich <michael.mulich@gmail.com>
2014-03-19 12:35:17 -0400

    Commitss pending documents to the archive.

---
+++ cnxpublishing/publish.py
@@ -0,0 +1,210 @@
+# -*- coding: utf-8 -*-
+# ###
+# Copyright (c) 2013, Rice University
+# This software is subject to the provisions of the GNU Affero General
+# Public License version 3 (AGPLv3).
+# See LICENCE.txt for details.
+# ###
+"""\
+Functions used to commit publication works to the archive.
+"""
+from cnxepub import Document, Binder
+from cnxarchive.utils import split_ident_hash
+
+from .utils import parse_user_uri
+
+
+__all__ = ('publish_model',)
+
+
+MODULE_INSERTION_TEMPLATE = """\
+WITH abstract_insertion AS (
+  INSERT INTO abstracts (abstractid, abstract, html)
+  VALUES (DEFAULT, %(summary)s, %(summary)s)
+  RETURNING abstractid),
+license_lookup AS (
+  SELECT licenseid
+  FROM licenses
+  WHERE url = %(license_url)s),
+module_insertion AS (
+  INSERT INTO modules
+    (uuid, major_version, minor_version,
+     module_ident, portal_type, moduleid,
+     name, created, revised, language,
+     submitter, submitlog,
+     abstractid,
+     licenseid,
+     parent, parentauthors,
+     authors, maintainers, licensors,
+     google_analytics, buylink,
+     stateid, doctype)
+  VALUES
+    ({__uuid__}, {__major_version__}, {__minor_version__},
+     DEFAULT, %(_portal_type)s, DEFAULT,
+     %(title)s, %(created)s, %(revised)s, %(language)s,
+     %(publisher)s, %(publication_message)s,
+     (SELECT abstractid FROM abstract_insertion),
+     (SELECT licenseid FROM license_lookup),
+     DEFAULT, DEFAULT,
+     %(authors)s, DEFAULT, %(copyright_holders)s,
+     DEFAULT, DEFAULT,
+     DEFAULT, ' ')
+  RETURNING
+    module_ident,
+    uuid||'@'||concat_ws('.',major_version,minor_version) AS ident_hash),
+author_roles AS (
+  INSERT INTO moduleoptionalroles (module_ident, roleid, personids)
+  VALUES ((SELECT module_ident FROM module_insertion), 1, %(authors)s)),
+licensor_roles AS (
+  INSERT INTO moduleoptionalroles
+    (module_ident, roleid, personids)
+  VALUES
+    ((SELECT module_ident FROM module_insertion), 2, %(copyright_holders)s)),
+translator_roles AS (
+  INSERT INTO moduleoptionalroles
+    (module_ident, roleid, personids)
+  VALUES
+    ((SELECT module_ident FROM module_insertion), 4, %(translators)s)),
+editor_roles AS (
+  INSERT INTO moduleoptionalroles
+    (module_ident, roleid, personids)
+  VALUES
+    ((SELECT module_ident FROM module_insertion), 5, %(editors)s))
+SELECT ident_hash FROM module_insertion
+"""
+
+# Constructs a table for mapping file data, filename
+# and metadata. It should look like this:
+#
+# row_number | filename | mimetype
+# ------------+------------------+------------
+# 1 | a.txt | text/plain
+# ------------+------------------+------------
+# 2 | index.cnxml.html | text/html
+#
+# This table is used for joining with the result from file_insertion:
+#
+# fileid | md5 | row_number
+# --------+----------------------------------+------------
+# 19 | 0ab908a54aa2e7e8ae52f7cfd92c79f4 | 1
+# --------+----------------------------------+------------
+# 20 | 11ddd7ab03313249c3bca3aba8cdc257 | 2
+#
+# So in the end we'll have a table with all fields:
+# fileid, md5, row_number, filename and mimetype
+MODULE_FILES_INSERTION_TEMPLATE = """\
+WITH module AS (
+  SELECT module_ident
+  FROM modules
+  WHERE uuid||'@'||concat_ws('.',major_version,minor_version) = %(ident_hash)s
+  ),
+file_insertion1 AS (
+  -- Note, the md5 hash is generated on insertion through a trigger.
+  INSERT INTO files (file)
+  VALUES {file_values}
+  RETURNING fileid, md5
+  ),
+file_insertion AS (
+  SELECT *, ROW_NUMBER() OVER () FROM file_insertion1
+  ),
+module_file_values AS ({module_file_values_sql}),
+module_file_insertion AS (
+  INSERT INTO module_files (module_ident, fileid, filename, mimetype)
+  SELECT module_ident, fileid, filename, mimetype
+  FROM
+    module,
+    file_insertion NATURAL JOIN module_file_values
+  RETURNING fileid, filename
+  )
+SELECT filename, md5
+FROM file_insertion f
+JOIN module_file_insertion mf ON f.fileid = mf.fileid
+"""
+
+TREE_NODE_INSERT = """
+INSERT INTO trees
+  (nodeid, parent_id, documentid,
+   title, childorder, latest)
+VALUES
+  (DEFAULT, %(parent_id)s, %(document_id)s,
+   %(title)s, %(child_order)s, %(is_latest)s)
+RETURNING nodeid
+"""
+
+
+def _model_to_portaltype(model):
+    if isinstance(model, Document):
+        type_ = 'Module'
+    elif isinstance(model, Binder):
+        type_ = 'Collection'
+    else:
+        raise ValueError("Unknown type: {}".format(type(model)))
+    return type_
+
+
+def _insert_metadata(cursor, model, publisher, message):
+    """Insert a module with the given ``metadata``."""
+    params = model.metadata.copy()
+    params['publisher'] = publisher
+    params['publication_message'] = message
+    params['_portal_type'] = _model_to_portaltype(model)
+    params['authors'] = [parse_user_uri(x['id']) for x in params['authors']]
+
+    # Assign the id and version if one is known.
+    if model.ident_hash is not None:
+        uuid, version = split_ident_hash(model.ident_hash,
+                                         split_version=True)
+        params['_uuid'] = uuid
+        params['_major_version'], params['_minor_version'] = version
+        # Format the statement to accept the identifiers.
+        stmt = MODULE_INSERTION_TEMPLATE.format(**{
+            '__uuid__': "%(_uuid)s::uuid",
+            '__major_version__': "%(_major_version)s",
+            '__minor_version__': "%(_minor_version)s",
+            })
+    else:
+        # Format the statement for defaults.
+        stmt = MODULE_INSERTION_TEMPLATE.format(**{
+            '__uuid__': "DEFAULT",
+            '__major_version__': "DEFAULT",
+            '__minor_version__': "DEFAULT",
+            })
+
+    cursor.execute(stmt, params)
+    ident_hash = cursor.fetchone()[0]
+    return ident_hash
+
+
+def _insert_files(cursor, ident_hash, files):
+    """Insert files and relates them to a document.
+    ``files`` should be a list of dicts::
+
+        [{'filename': 'a.txt', 'mimetype': 'text/plain',
+          'data': 'content of a.txt'},
+         ...]
+
+    This returns a list of filenames and hashes.
+    """
+    raise NotImplementedError()
+
+
+def _insert_tree(cursor, tree, parent_id=None, index=0):
+    """Inserts a binder tree into the archive."""
+    raise NotImplementedError()
+
+
+def publish_model(cursor, model, publisher, message):
+    """Publishes the ``model`` and return its ident_hash."""
+    publishers = publisher
+    if isinstance(publishers, list) and len(publishers) > 1:
+        raise ValueError("Only one publisher is allowed. '{}' "
+                         "were given: {}" \
+                         .format(len(publishers), publishers))
+    ident_hash = _insert_metadata(cursor, model, publisher, message)
+    if isinstance(model, Document):
+        files = []  # TODO
+        file_hashes = _insert_files(cursor, ident_hash, files)
+    elif isinstance(model, Binder):
+        tree = {}  # TODO
+        tree = _insert_tree(cursor, tree)
+    return ident_hash
---
+++ cnxpublishing/tests/test_publish.py
@@ -0,0 +1,225 @@
+# -*- coding: utf-8 -*-
+# ###
+# Copyright (c) 2013, Rice University
+# This software is subject to the provisions of the GNU Affero General
+# Public License version 3 (AGPLv3).
+# See LICENCE.txt for details.
+# ###
+import os
+import io
+import datetime
+import uuid
+import unittest
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+import cnxepub
+import psycopg2
+from cnxarchive.utils import join_ident_hash
+from webob import Request
+from pyramid import testing
+from pyramid.paster import get_appsettings
+
+from .test_db import integration_test_settings
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def _db_connection_factory(connection_string=None):
+    if connection_string is None:
+        settings = integration_test_settings()
+        from ..config import CONNECTION_STRING
+        connection_string = settings[CONNECTION_STRING]
+
+    def db_connect():
+        return psycopg2.connect(connection_string)
+
+    return db_connect
+
+
+class PublishUtilityTestCase(unittest.TestCase):
+    """Unit tests for publish specific utilities."""
+
+    def test_model_to_portaltype(self):
+        """Model types to legacy portal type names."""
+        from ..publish import _model_to_portaltype as target
+        document = cnxepub.Document.__new__(cnxepub.Document)
+        self.assertEqual(target(document), 'Module')
+        binder = cnxepub.Binder.__new__(cnxepub.Binder)
+        self.assertEqual(target(binder), 'Collection')
+        ugly = object()
+        with self.assertRaises(ValueError):
+            target(ugly)
+
+
+class PublishIntegrationTestCase(unittest.TestCase):
+    """Verify publication interactions with the archive database."""
+
+    settings = None
+    db_conn_str = None
+    db_connect = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.settings = integration_test_settings()
+        from ..config import CONNECTION_STRING
+        cls.db_conn_str = cls.settings[CONNECTION_STRING]
+        cls.db_connect = staticmethod(_db_connection_factory())
+        # FIXME psycopg2 UUID adaptation doesn't seem to be registering
+        # itself. Temporarily call it directly.
+        from psycopg2.extras import register_uuid
+        register_uuid()
+
+    def setUp(self):
+        from cnxarchive.database import initdb
+        initdb({'db-connection-string': self.db_conn_str})
+        from ..db import initdb
+        initdb(self.db_conn_str)
+        self.config = testing.setUp(settings=self.settings)
+
+    def tearDown(self):
+        with psycopg2.connect(self.db_conn_str) as db_conn:
+            with db_conn.cursor() as cursor:
+                cursor.execute("DROP SCHEMA public CASCADE")
+                cursor.execute("CREATE SCHEMA public")
+        testing.tearDown()
+
+    def make_document(self, id=None, content=None, metadata=None):
+        if content is None:
+            content = io.BytesIO(b'<p>Blank.</p>')
+        document = cnxepub.Document(id, content,
+                                    metadata=metadata)
+        return document
+
+    def test_document_insertion_wo_id_n_version(self):
+        """\
+        Check that document insertion code inserts a documents's metadata
+        into the archive.
+        The scope of this test and its function is the metadata,
+        not the content associated with it.
+        """
+        metadata = {
+            'title': "Dingbat's Dilemma",
+            'language': 'en-us',
+            'summary': "The options are limitless.",
+            'created': '1420-02-03 23:36:20.583149-05',
+            'revised': '1420-02-03 23:36:20.583149-05',
+            'license_url': 'http://creativecommons.org/licenses/by/3.0/',
+            'publishers': ['ream'],  # XXX We don't have a mapping.
+            'authors': [{'id': 'rbates', 'type': 'cnx-id',
+                         'name': 'Richard Bates'},],
+            'editors': ['jone', 'kahn'],
+            'illustrators': ['AbagaleBates'],  # XXX We don't have a mapping.
+            'translators': ['RhowandaOkofarBates', 'JamesOrwel'],
+            'copyright_holders': ['ream'],
+            'subjects': ['Business', 'Arts', 'Mathematics and Statistics'],
+            'keywords': ['dingbat', 'bates', 'dilemma'],
+            }
+        publisher = 'ream'
+        message = 'no msg'
+        document = self.make_document(metadata=metadata)
+
+        from ..publish import _insert_metadata
+        with self.db_connect() as db_conn:
+            with db_conn.cursor() as cursor:
+                ident_hash = _insert_metadata(cursor, document,
+                                              publisher, message)
+
+        with self.db_connect() as db_conn:
+            with db_conn.cursor() as cursor:
+                cursor.execute("""\
+SELECT
+  m.name, m.language, a.abstract, l.url,
+  m.major_version, m.minor_version,
+  m.authors, m.submitter, m.submitlog, m.maintainers,
+  m.licensors, m.parentauthors, m.google_analytics, m.buylink
+FROM
+  modules AS m
+  NATURAL JOIN abstracts AS a
+  LEFT JOIN licenses AS l ON m.licenseid = l.licenseid
+WHERE m.uuid||'@'||concat_ws('.',m.major_version,m.minor_version) = %s
+""", (ident_hash,))
+                module = cursor.fetchone()
+
+        self.assertEqual(module[0], metadata['title'])
+        self.assertEqual(module[1], metadata['language'])
+        self.assertEqual(module[2], metadata['summary'])
+        self.assertEqual(module[3], metadata['license_url'])
+        self.assertEqual(module[4], 1)
+        self.assertEqual(module[5], None)
+        self.assertEqual(module[6], [x['id'] for x in metadata['authors']])
+        self.assertEqual(module[7], publisher)
+        self.assertEqual(module[8], message)
+        self.assertEqual(module[9], None)  # TODO maintainers list?
+        self.assertEqual(module[10], metadata['copyright_holders'])
+        self.assertEqual(module[11], None)  # TODO parent authors?
+        self.assertEqual(module[12], None)  # TODO analytics code?
+        self.assertEqual(module[13], None)  # TODO buy link?
+
+        # Check the roles...
+        with self.db_connect() as db_conn:
+            with db_conn.cursor() as cursor:
+                cursor.execute("""\
+WITH module AS (
+  SELECT module_ident
+  FROM modules AS m
+  WHERE m.uuid||'@'||m.major_version = %s)
+SELECT r.roleparam, mor.personids
+FROM moduleoptionalroles AS mor NATURAL JOIN roles AS r
+WHERE mor.module_ident = (SELECT module_ident from module)
+""", (ident_hash,))
+                roles = dict(cursor.fetchall())
+        self.assertEqual(roles['authors'],
+                         [x['id'] for x in metadata['authors']])
+        self.assertEqual(roles['licensors'], metadata['copyright_holders'])
+        self.assertEqual(roles['translators'], metadata['translators'])
+
+    def test_document_insertion_w_id_n_version_provided(self):
+        id, version = '3a70f722-b7b0-4b41-83dd-2790cee98c39', '1'
+        expected_ident_hash = join_ident_hash(id, version)
+        metadata = {
+            'version': version,
+            'title': "Dingbat's Dilemma",
+            'language': 'en-us',
+            'summary': "The options are limitless.",
+            'created': '1420-02-03 23:36:20.583149-05',
+            'revised': '1420-02-03 23:36:20.583149-05',
+            'license_url': 'http://creativecommons.org/licenses/by/3.0/',
+            'publishers': ['ream'],  # XXX We don't have a mapping.
+            'authors': [{'id': 'rbates', 'type': 'cnx-id',
+                         'name': 'Richard Bates'},],
+            'editors': ['jone', 'kahn'],
+            'illustrators': ['AbagaleBates'],  # XXX We don't have a mapping.
+            'translators': ['RhowandaOkofarBates', 'JamesOrwel'],
+            'copyright_holders': ['ream'],
+            'subjects': ['Business', 'Arts', 'Mathematics and Statistics'],
+            'keywords': ['dingbat', 'bates', 'dilemma'],
+            }
+        publisher = 'ream'
+        message = 'no msg'
+        document = self.make_document(id=id, metadata=metadata)
+
+        from ..publish import _insert_metadata
+        with self.db_connect() as db_conn:
+            with db_conn.cursor() as cursor:
+                ident_hash = _insert_metadata(cursor, document,
+                                              publisher, message)
+
+        self.assertEqual(ident_hash, expected_ident_hash)
+
+        with self.db_connect() as db_conn:
+            with db_conn.cursor() as cursor:
+                cursor.execute("""\
+SELECT m.name, uuid::text, m.major_version, m.minor_version
+FROM modules AS m
+WHERE m.uuid||'@'||concat_ws('.',m.major_version,m.minor_version) = %s
+""", (ident_hash,))
+                module = cursor.fetchone()
+
+        self.assertEqual(module[0], metadata['title'])
+        self.assertEqual(module[1], id)
+        self.assertEqual(module[2], int(version))
+        self.assertEqual(module[3], None)
