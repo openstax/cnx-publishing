631e13383e73e0941cddb6150816dadf1333b35a
karen chan <karen@karen-chan.com>
2015-01-09 14:38:01 +0100

    Release version 0.2.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ if not IS_PY3:
 
 setup(
     name='cnx-publishing',
-    version='0.1',
+    version='0.2.0',
     author='Connexions team',
     author_email='info@cnx.org',
     url="https://github.com/connexions/cnx-publishing",
