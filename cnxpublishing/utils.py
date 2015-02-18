14a9e7b13e4dc56c3c73d81b74f8d9e5b6420511
Michael Mulich <michael.mulich@gmail.com>
2015-02-18 13:25:11 -0500

    Add tests for utils.

--- cnxpublishing/utils.py
+++ cnxpublishing/utils.py
@@ -24,11 +24,7 @@ def parse_archive_uri(uri):
     """Given an archive URI, parse to a split ident-hash."""
     parsed = urlparse(uri)
     path = parsed.path.rstrip('/').split('/')
-    try:
-        ident_hash = path[-1]
-    except:
-        raise ValueError("Expected a path like /contents/{ident_hash}, "
-                         "got '{}' instead.".format(path))
+    ident_hash = path[-1]
     ident_hash = unquote(ident_hash)
     return ident_hash
 
