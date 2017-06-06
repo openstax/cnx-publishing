5d9e40571a008a14273e31cd26bee65cf9ce9e42
Michael Mulich <michael.mulich@gmail.com>
2017-06-06 10:30:12 -0700

    Rewrite subscriber tests using pytest methods
    This was done to better incorporate the Celery implemenation.
    Celery provides pytest fixtures. This will enable us to use
    their fixtures in our tests.

--- cnxpublishing/tests/test_subscribers.py
+++ cnxpublishing/tests/test_subscribers.py
@@ -1,74 +1,54 @@
 # -*- coding: utf-8 -*-
 import json
 import time
-import unittest
-try:
-    from unittest import mock
-except ImportError:
-    import mock
 
+import pytest
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-from . import use_cases
-from .testing import db_connect
-from .test_db import BaseDatabaseIntegrationTestCase
+
+@pytest.fixture
+def channel_processing_start_up_event():
+    from cnxpublishing.events import ChannelProcessingStartUpEvent
+    event = ChannelProcessingStartUpEvent()
+    return event
 
 
-class BaseSubscriberTestCase(BaseDatabaseIntegrationTestCase):
+@pytest.mark.usefixtures('publishing_app')
+def test_startup_event(db_cursor, complex_book_one,
+                       channel_processing_start_up_event):
+    cursor = db_cursor
+    book_one, ident_mapping = complex_book_one
+    # Start listening for post_publication notifications.
+    cursor.connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor.execute('LISTEN post_publication')
+    cursor.connection.commit()
 
-    @db_connect
-    def setUp(self, cursor):
-        super(BaseSubscriberTestCase, self).setUp()
-        self.binder = use_cases.setup_COMPLEX_BOOK_ONE_in_archive(self, cursor)
+    from cnxpublishing.subscribers import post_publication_start_up
+    post_publication_start_up(channel_processing_start_up_event)
+    # Slowish machines require some time to catch up
+    time.sleep(0.5)
+
+    # Commit and poll to get the notifications
+    cursor.connection.commit()
+    cursor.connection.poll()
+    try:
+        notify = cursor.connection.notifies.pop(0)
+    except IndexError:
+        pytest.fail("the target did not create any notifications")
+
+    # Check that a notification was sent.
+    payload = json.loads(notify.payload)
+    assert book_one.ident_hash in payload['ident_hash']
+    assert ident_mapping[book_one.ident_hash] == payload['module_ident']
+
+
+class TestPostPublicationProcessing(object):
+
+    @pytest.fixture(autouse=True)
+    def suite_fixture(self, complex_book_one):
+        self.binder, ident_mapping = complex_book_one
         self.ident_hash = self.binder.ident_hash
-        cursor.execute(
-            "SELECT module_ident FROM modules "
-            "WHERE ident_hash(uuid, major_version, minor_version) = %s",
-            (self.ident_hash,))
-        self.module_ident = cursor.fetchone()[0]
-
-
-class PostPublicationStartUpTestCase(BaseSubscriberTestCase):
-
-    @property
-    def target(self):
-        from cnxpublishing.subscribers import post_publication_start_up
-        return post_publication_start_up
-
-    def make_event(self):
-        from cnxpublishing.events import ChannelProcessingStartUpEvent
-        event = ChannelProcessingStartUpEvent()
-        return event
-
-    def call_target(self):
-        self.target(self.make_event())
-
-    @db_connect
-    def test(self, cursor):
-        # Start listening for post_publication notifications.
-        cursor.connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-        cursor.execute('LISTEN post_publication')
-        cursor.connection.commit()
-
-        self.call_target()
-        # Slowish machines require some time to catch up
-        time.sleep(0.5)
-
-        # Commit and poll to get the notifications
-        cursor.connection.commit()
-        cursor.connection.poll()
-        try:
-            notify = cursor.connection.notifies.pop(0)
-        except IndexError:
-            self.fail("the target did not create any notifications")
-
-        # Check that a notification was sent.
-        payload = json.loads(notify.payload)
-        self.assertEqual(self.module_ident, payload['module_ident'])
-        self.assertIn(self.ident_hash, payload['ident_hash'])
-
-
-class PostPublicationProcessingTestCase(BaseSubscriberTestCase):
+        self.module_ident = ident_mapping[self.binder.ident_hash]
 
     @property
     def target(self):
@@ -97,34 +77,32 @@ class PostPublicationProcessingTestCase(BaseSubscriberTestCase):
     # We don't test for not found, because a notify only takes place when
     #   a module exists.
 
-    @db_connect
-    def test(self, cursor):
+    def test(self, db_cursor):
         # Set up (setUp) creates the content, thus putting it in the
         # post-publication state. We simply create the event associated
         # with that state change.
         event = self.make_event()
 
         self.target(event)
-        cursor.execute("SELECT count(*) FROM trees WHERE is_collated = 't';")
-        collation_count = cursor.fetchone()[0]
+        db_cursor.execute("SELECT count(*) FROM trees WHERE is_collated = 't';")
+        collation_count = db_cursor.fetchone()[0]
         assert collation_count > 0, "baking didn't happen"
 
-        cursor.execute("SELECT ms.statename "
+        db_cursor.execute("SELECT ms.statename "
                        "FROM modules AS m NATURAL JOIN modulestates AS ms "
                        "WHERE module_ident = %s",
                        (self.module_ident,))
-        self.assertEqual(cursor.fetchone()[0], 'current')
+        assert db_cursor.fetchone()[0] == 'current'
 
         # We check for all the states because the timestamps are exactly
         # the same since the processes run almost in sequence.
-        cursor.execute("SELECT state FROM post_publications "
+        db_cursor.execute("SELECT state FROM post_publications "
                        "WHERE module_ident = %s "
                        "ORDER BY timestamp DESC",
                        (self.module_ident,))
-        self.assertIn('Done/Success', [r[0] for r in cursor.fetchall()])
+        assert 'Done/Success' in [r[0] for r in db_cursor.fetchall()]
 
-    @db_connect
-    def test_state_updated_midway(self, cursor):
+    def test_state_updated_midway(self, db_cursor, mocker):
         from cnxarchive.scripts.export_epub import factory
 
         class Patcher(object):
@@ -133,12 +111,12 @@ class PostPublicationProcessingTestCase(BaseSubscriberTestCase):
                 self.captured_state = None
 
             def __call__(self, *args, **kwargs):
-                cursor.execute("SELECT ms.statename "
+                db_cursor.execute("SELECT ms.statename "
                                "FROM modules AS m "
                                "NATURAL JOIN modulestates AS ms "
                                "WHERE module_ident = %s",
                                (self.module_ident,))
-                self.captured_state = cursor.fetchone()[0]
+                self.captured_state = db_cursor.fetchone()[0]
                 return factory(*args, **kwargs)
 
         # Set up (setUp) creates the content, thus putting it in the
@@ -148,19 +126,18 @@ class PostPublicationProcessingTestCase(BaseSubscriberTestCase):
 
         # Before we bake assert the state has changed.
         patched = Patcher(self.module_ident)
-        patch_path = 'cnxarchive.scripts.export_epub.factory'
-        with mock.patch(patch_path, new=patched):
-            self.target(event)
+        mocker.patch('cnxarchive.scripts.export_epub.factory', new=patched)
 
-        self.assertEqual(patched.captured_state, 'processing')
+        self.target(event)
 
-    @db_connect
-    @mock.patch('cnxpublishing.subscribers.bake')
-    def test_error_handling_of_unknown_error(self, cursor, mock_bake):
+        assert patched.captured_state == 'processing'
+
+    def test_error_handling_of_unknown_error(self, db_cursor, mocker):
 
         def bake(*args, **kwargs):
             raise Exception('something failed during baking')
 
+        mock_bake = mocker.patch('cnxpublishing.subscribers.bake')
         mock_bake.side_effect = bake
 
         # Set up (setUp) creates the content, thus putting it in the
@@ -171,27 +148,26 @@ class PostPublicationProcessingTestCase(BaseSubscriberTestCase):
         self.target(event)
 
         # Make sure it is marked as 'errored'.
-        cursor.execute("SELECT ms.statename "
+        db_cursor.execute("SELECT ms.statename "
                        "FROM modules AS m NATURAL JOIN modulestates AS ms "
                        "WHERE module_ident = %s",
                        (self.module_ident,))
-        self.assertEqual(cursor.fetchone()[0], 'errored')
+        db_cursor.fetchone()[0] == 'errored'
 
         # Make sure the post_publication state is maked as 'Failed/Error'.
-        cursor.execute("SELECT state, state_message FROM post_publications "
+        db_cursor.execute("SELECT state, state_message FROM post_publications "
                        "WHERE module_ident = %s",
                        (self.module_ident,))
         from cnxpublishing.subscribers import CONTACT_SITE_ADMIN_MESSAGE
-        self.assertIn(('Failed/Error', CONTACT_SITE_ADMIN_MESSAGE,),
-                      [r for r in cursor.fetchall()])
+        expected = ('Failed/Error', CONTACT_SITE_ADMIN_MESSAGE,)
+        assert expected in [r for r in db_cursor.fetchall()]
 
-    @db_connect
-    @mock.patch('cnxarchive.scripts.export_epub.factory')
-    def test_error_handling_during_epub_export(self, cursor, mock_factory):
+    def test_error_handling_during_epub_export(self, db_cursor, mocker):
 
         def factory(*args, **kwargs):
             raise Exception('something went wrong during export')
 
+        mock_factory = mocker.patch('cnxarchive.scripts.export_epub.factory')
         mock_factory.side_effect = factory
 
         # Set up (setUp) creates the content, thus putting it in the
@@ -202,16 +178,16 @@ class PostPublicationProcessingTestCase(BaseSubscriberTestCase):
         self.target(event)
 
         # Make sure it is marked as 'errored'.
-        cursor.execute("SELECT ms.statename "
+        db_cursor.execute("SELECT ms.statename "
                        "FROM modules AS m NATURAL JOIN modulestates AS ms "
                        "WHERE module_ident = %s",
                        (self.module_ident,))
-        self.assertEqual(cursor.fetchone()[0], 'errored')
+        db_cursor.fetchone()[0] == 'errored'
 
         # Make sure the post_publication state is maked as 'Failed/Error'.
-        cursor.execute("SELECT state, state_message FROM post_publications "
+        db_cursor.execute("SELECT state, state_message FROM post_publications "
                        "WHERE module_ident = %s",
                        (self.module_ident,))
         from cnxpublishing.subscribers import CONTACT_SITE_ADMIN_MESSAGE
-        self.assertIn(('Failed/Error', CONTACT_SITE_ADMIN_MESSAGE,),
-                      [r for r in cursor.fetchall()])
+        expected = ('Failed/Error', CONTACT_SITE_ADMIN_MESSAGE,)
+        assert expected in [r for r in db_cursor.fetchall()]
