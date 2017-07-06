a3773968e1782c5e2d9a12088a85e56f5263fa0c
sisiz <zimmerman.sisi@gmail.com>
2017-07-06 13:46:48 -0500

    change javascript to use a form instead

--- cnxpublishing/tests/views/test_admin.py
+++ cnxpublishing/tests/views/test_admin.py
@@ -257,7 +257,6 @@ class ContentStatusViewsTestCase(unittest.TestCase):
 
     def test_admin_content_status_no_filters(self):
         request = testing.DummyRequest()
-
         from ...views.admin import admin_content_status
         content = admin_content_status(request)
         self.assertEqual({
@@ -270,7 +269,7 @@ class ContentStatusViewsTestCase(unittest.TestCase):
             'page': 1,
             'num_entries': 100,
             'sort': 'bpsa.created DESC',
-            'sort_created': 'fa fa-angle-down',
+            'newSort': 'selected',
             'states': content['states']
         }, content)
         self.assertEqual(len(content['states']), 2)
@@ -283,9 +282,9 @@ class ContentStatusViewsTestCase(unittest.TestCase):
 
         request.GET = {'page': 1,
                        'number': 2,
-                       'sort': 'STATE ASC',
+                       'sort': 'STATE',
                        'author': 'charrose',
-                       'exculde_statuses': 'SUCCESS,STARTED'}
+                       'status_filter': ['FAILURE', 'RETRY', 'PENDING']}
         from ...views.admin import admin_content_status
         content = admin_content_status(request)
         self.assertEqual({
@@ -296,8 +295,8 @@ class ContentStatusViewsTestCase(unittest.TestCase):
             'page': 1,
             'num_entries': 2,
             'author': 'charrose',
-            'sort': 'STATE ASC',
-            'sort_state': 'fa fa-angle-up',
+            'sort': 'bpsa.created DESC',
+            'stateSort': 'selected',
             'states': content['states']
         }, content)
         self.assertEqual(len(content['states']), 2)
