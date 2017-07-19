857f8c5c37995f52f01a68db80866738b32e0ad1
Ross Reedstrom <reedstrm@gmail.com>
2017-07-19 18:17:57 -0500

    fixup tests for change in bake() function signature

--- cnxpublishing/tests/test_bake.py
+++ cnxpublishing/tests/test_bake.py
@@ -72,11 +72,11 @@ WHERE
             binder_model.append(composite_section)
             return binder_model
 
-        fake_recipe = 'div::after { content: "test" }'
+        fake_recipe_id = 1
 
         with mock.patch('cnxpublishing.bake.collate_models') as mock_collate:
             mock_collate.side_effect = cnxepub_collate
-            errors = self.target(binder, fake_recipe, publisher, msg)
+            errors = self.target(binder, fake_recipe_id, publisher, msg)
 
         # Ensure the output of the errors.
         self.assertEqual(errors, [])
@@ -162,8 +162,8 @@ WHERE
         with mock.patch('cnxpublishing.bake.collate_models') as mock_collate:
             mock_collate.side_effect = cnxepub_collate
             from cnxpublishing.bake import bake
-            fake_recipe = 'div::after { content: "test" }'
-            errors = bake(binder, fake_recipe, publisher, msg, cursor=cursor)
+            fake_recipe_id = 1
+            errors = bake(binder, fake_recipe_id, publisher, msg, cursor=cursor)
 
         self.ident_hash = binder.ident_hash
         self.composite_ident_hash = composite_doc.ident_hash
