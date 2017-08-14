c00af6bdbf71c9eba337d0313006e534c5050ca7
Philip Schatz <phil@cnx.org>
2017-08-14 10:56:58 -0500

    :art: change dates to be relative

--- cnxpublishing/static/js/main.js
+++ cnxpublishing/static/js/main.js
@@ -1,1 +1,6 @@
 
+// Change dates to be relative
+$('datetime:not([title]):first-child').each(function(index, el) {
+  el.setAttribute('title', el.textContent)
+  el.textContent = window.moment(el.textContent).fromNow()
+})
