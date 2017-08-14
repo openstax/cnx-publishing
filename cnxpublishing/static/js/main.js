af16c71b9eea6d1ba256295471609a8429422df8
Philip Schatz <phil@cnx.org>
2017-08-14 12:00:07 -0400

    :bug: typo

--- cnxpublishing/static/js/main.js
+++ cnxpublishing/static/js/main.js
@@ -1,6 +1,6 @@
 
 // Change dates to be relative
-$('datetime:not([title]):first-child').each(function(index, el) {
+$('datetime:not([title])').each(function(index, el) {
   el.setAttribute('title', el.textContent)
   el.textContent = window.moment(el.textContent).fromNow()
 })
