82d9c54f455b1550861af503f22dcfa3c6c5766f
Ross Reedstrom <reedstrm@gmail.com>
2017-12-04 14:57:32 -0600

    change how URI triggers bake - force notify even if alreay in post_publication state

--- cnxpublishing/views/publishing.py
+++ cnxpublishing/views/publishing.py
@@ -251,19 +251,27 @@ def bake_content(request):
     with db_connect() as db_conn:
         with db_conn.cursor() as cursor:
             cursor.execute("""\
-SELECT bool(portal_type = 'Collection')
+SELECT bool(portal_type = 'Collection'), stateid, module_ident
 FROM modules
 WHERE ident_hash(uuid, major_version, minor_version) = %s
 """, (ident_hash,))
             try:
-                is_binder = cursor.fetchone()[0]
+                is_binder, stateid, module_ident = cursor.fetchone()
             except TypeError:
                 raise httpexceptions.HTTPNotFound()
             if not is_binder:
                 raise httpexceptions.HTTPBadRequest(
                     '{} is not a book'.format(ident_hash))
 
-            cursor.execute("""\
+            if stateid == 5:
+                cursor.execute("""\
+SELECT pg_notify('post_publication',
+'{"module_ident": '||%s||',
+  "ident_hash": "'||%s||'",
+  "timestamp": "'||CURRENT_TIMESTAMP||'"}')
+""", (module_ident, ident_hash))
+            else:
+                cursor.execute("""\
 UPDATE modules SET stateid = 5
 WHERE ident_hash(uuid, major_version, minor_version) = %s
 """, (ident_hash,))
