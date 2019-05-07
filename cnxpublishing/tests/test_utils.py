eb0bdb304b7701deb2373238f08da813d1b5ac02
Michael Mulich <michael.mulich@gmail.com>
2019-05-07 12:23:10 -0700

    Add utility func to amend the tree with slug values

--- cnxpublishing/tests/test_utils.py
+++ cnxpublishing/tests/test_utils.py
@@ -7,6 +7,8 @@
 # ###
 import unittest
 
+from cnxpublishing.utils import amend_tree_with_slugs
+
 
 class ParseArchiveUriTestCase(unittest.TestCase):
 
@@ -41,3 +43,114 @@ class ParseUserUriTestCase(unittest.TestCase):
         self.assertEqual(
             caught_exc.exception.args[0],
             "Can't parse a user uri of type '{}'.".format(invalid_type))
+
+
+def test_amend_tree_with_slugs():
+    # This tree struct only contains the required parts,
+    # where id, shortid, etc. are ignored.
+    tree = {
+        'title': 'Book of Origin',
+        'contents': [
+            {'title': '<span class="os-text">Preface</span>'},
+            {'title': (
+                '<span class="os-number">1</span>'
+                '<span class="os-divider"> </span>'
+                '<span class="os-text">Chapter One</span>'),
+             'contents': [
+                 {'title': (
+                     '<span class="os-number">1.1</span>'
+                     '<span class="os-divider"> </span>'
+                     '<span class="os-text">Apple</span>')},
+                 {'title': (
+                     '<span class="os-number">1.2</span>'
+                     '<span class="os-divider"> </span>'
+                     '<span class="os-text">Banana</span>')},
+                 {'title': (
+                     '<span class="os-text">'
+                     'Problems &amp; Exercises'
+                     '</span>')}],
+             },
+            {'title': (
+                '<span class="os-number">2</span>'
+                '<span class="os-divider"> </span>'
+                '<span class="os-text">Chapter One</span>'),
+             'contents': [
+                 {'title': (
+                     '<span class="os-number">1.1</span>'
+                     '<span class="os-divider"> </span>'
+                     '<span class="os-text">Apple</span>')},
+                 {'title': (
+                     '<span class="os-number">1.2</span>'
+                     '<span class="os-divider"> </span>'
+                     '<span class="os-text">Banana</span>')},
+                 {'title': (
+                     '<span class="os-text">'
+                     'Key Terms'
+                     '</span>')}],
+             },
+            {'title': '<span class="os-text">Index</span>'},
+        ],
+    }
+
+    # Call the target
+    amend_tree_with_slugs(tree)
+
+    expected_tree = {
+        'title': 'Book of Origin',
+        'slug': 'book-of-origin',
+        'contents': [
+            {'slug': 'preface',
+             'title': '<span class="os-text">Preface</span>'},
+            {'title': (
+                '<span class="os-number">1</span>'
+                '<span class="os-divider"> </span>'
+                '<span class="os-text">Chapter One</span>'
+             ),
+             'slug': '1-chapter-one',
+             'contents': [
+                {'slug': '1-1-apple',
+                 'title': (
+                     '<span class="os-number">1.1</span>'
+                     '<span class="os-divider"> </span>'
+                     '<span class="os-text">Apple</span>'
+                 )},
+                {'slug': '1-2-banana',
+                 'title': (
+                     '<span class="os-number">1.2</span>'
+                     '<span class="os-divider"> </span>'
+                     '<span class="os-text">Banana</span>'
+                 )},
+                {'slug': '1-problems-exercises',
+                 'title': (
+                     '<span class="os-text">'
+                     'Problems &amp; Exercises'
+                     '</span>'
+                 )}]
+             },
+            {'title': (
+                '<span class="os-number">2</span>'
+                '<span class="os-divider"> </span>'
+                '<span class="os-text">Chapter One</span>'),
+             'slug': '2-chapter-one',
+             'contents': [
+                 {'slug': '1-1-apple',
+                  'title': (
+                      '<span class="os-number">1.1</span>'
+                      '<span class="os-divider"> </span>'
+                      '<span class="os-text">Apple</span>'
+                  )},
+                 {'slug': '1-2-banana',
+                  'title': (
+                      '<span class="os-number">1.2</span>'
+                      '<span class="os-divider"> </span>'
+                      '<span class="os-text">Banana</span>'
+                  )},
+                 {'slug': '2-key-terms',
+                  'title': '<span class="os-text">Key Terms</span>'}],
+             },
+            {'slug': 'index',
+             'title': '<span class="os-text">Index</span>'},
+        ],
+    }
+
+    assert tree == expected_tree
